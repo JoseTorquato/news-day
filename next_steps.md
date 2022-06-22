e0d2a7ca0eb87ab24f0cb699897c1329ae8bc10e
JoseTorquato <jltorquato12@gmail.com>
2022-06-22 08:26:44 -0300

    feat: create README base, create next_steps with 5W2H

---
+++ next_steps.md
@@ -0,0 +1,20 @@
+O que?
+O projeto vai ter como premissa buscar assuntos cadastrado pelo cliente e entrega de uma forma simples e resumida no email como newsletter.
+
+Por que? 
+Muitas das vezes gostariamos de acompanhas algum assunto, e por algum motivo ou correria do dia a dia não conseguimos pesquisar sobre aquele assunto.
+
+Onde?
+Inicialmente web
+
+Quando? 
+A definir via cronograma de etapas, mas a V0 até dia 01/07/22
+
+Quem?
+José Lucas 
+
+Como será feito?
+Será desenvolvido um software de web scrapping inicialmente em minha maquina local, com um banco para guardar meus assuntos e disparo do resumo via email.
+
+Quanto vai custar?
+Inicialmente 0 Reais, podendo claro escalar.
--- script.py
+++ script.py
@@ -1,18 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-SEARCH = "autismo".replace(" ", "%20")
-print(SEARCH)
+SUBJECT = "Scrapping".replace(" ", "%20")
+print(SUBJECT)
 # codigo para get principais noticias no google
 
 print("Googgle Search\n")
-URL = f"https://www.google.com/search?q={SEARCH}&ei=9uiyYuiqPIv41sQPpd29yA8&ved=0ahUKEwjo_Yqa58D4AhULvJUCHaVuD_kQ4dUDCA4&uact=5&oq=manha+perfeita&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsANKBAhBGABKBAhGGABQAFgAYNUIaANwAXgAgAEAiAEAkgEAmAEAyAEIwAEB&sclient=gws-wiz"
+URL = f"https://www.google.com/search?q={SUBJECT}&ei=9uiyYuiqPIv41sQPpd29yA8&ved=0ahUKEwjo_Yqa58D4AhULvJUCHaVuD_kQ4dUDCA4&uact=5&oq=manha+perfeita&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsANKBAhBGABKBAhGGABQAFgAYNUIaANwAXgAgAEAiAEAkgEAmAEAyAEIwAEB&sclient=gws-wiz"
 response = requests.get(URL, headers={"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'})
 soup = BeautifulSoup(response.content, "html.parser")
 news = soup.find_all("div", attrs={"class":"g tF2Cxc"})
 
 for new in news:
-    print(type(new))
     print(" ".join(new.h3.text.split()), "\n")
     print(new.a["href"])
     print()
@@ -22,10 +21,10 @@ print("\n",60*"-","\n")
 print("Google News\n")
 
 INDICE = 0
-PAGES = 50
+PAGES = 1
 COUNT = 0
 for i in range(PAGES):
-    URL2 = f"https://www.google.com/search?q={SEARCH}&hl=pt-BR&tbm=nws&ei=x-GyYqCFNPjk1sQP5927gAQ&start={INDICE}&sa=N&ved=2ahUKEwjghI-t4MD4AhV4spUCHefuDkA4ChDy0wN6BAgBEDw&biw=948&bih=730&dpr=2"
+    URL2 = f"https://www.google.com/search?q={SUBJECT}&hl=pt-BR&tbm=nws&ei=x-GyYqCFNPjk1sQP5927gAQ&start={INDICE}&sa=N&ved=2ahUKEwjghI-t4MD4AhV4spUCHefuDkA4ChDy0wN6BAgBEDw&biw=948&bih=730&dpr=2"
     response2 = requests.get(URL2, headers={"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
     })
     soup2 = BeautifulSoup(response2.content, "html.parser")
@@ -42,6 +41,3 @@ for i in range(PAGES):
     COUNT += len(news2)
 
 print("Content: ", COUNT)
-
-
-
