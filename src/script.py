1055e166170c9cb94ff0ca47e712fad0148f48b9
José Lucas <jltorquato12@gmail.com>
2022-06-23 09:23:23 -0300

    fix: removed pylint

--- src/script.py
+++ src/script.py
@@ -7,7 +7,12 @@ print(SUBJECT)
 
 print("Googgle Search\n")
 URL = f"https://www.google.com/search?q={SUBJECT}&ei=9uiyYuiqPIv41sQPpd29yA8&ved=0ahUKEwjo_Yqa58D4AhULvJUCHaVuD_kQ4dUDCA4&uact=5&oq=manha+perfeita&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsANKBAhBGABKBAhGGABQAFgAYNUIaANwAXgAgAEAiAEAkgEAmAEAyAEIwAEB&sclient=gws-wiz"
-response = requests.get(URL, headers={"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'})
+response = requests.get(
+    URL,
+    headers={
+        "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
+    })
+
 soup = BeautifulSoup(response.content, "html.parser")
 news = soup.find_all("div", attrs={"class":"g tF2Cxc"})
 
@@ -19,13 +24,15 @@ for new in news:
 # Get 4 noticies in google news
 print("\n",60*"-","\n")
 print("Google News\n")
-
 INDICE = 0
 PAGES = 1
 COUNT = 0
 for i in range(PAGES):
     URL2 = f"https://www.google.com/search?q={SUBJECT}&hl=pt-BR&tbm=nws&ei=x-GyYqCFNPjk1sQP5927gAQ&start={INDICE}&sa=N&ved=2ahUKEwjghI-t4MD4AhV4spUCHefuDkA4ChDy0wN6BAgBEDw&biw=948&bih=730&dpr=2"
-    response2 = requests.get(URL2, headers={"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
+    response2 = requests.get(
+        URL2,
+        headers={
+            "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
     })
     soup2 = BeautifulSoup(response2.content, "html.parser")
 
